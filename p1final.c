f62e2871b8eb4fd1f41af66ef386213c65ccfa6e
Gayathri K <95470777+rithvikteja@users.noreply.github.com>
2022-04-02 08:52:28 +0000

    find distance between two points

--- p1final.c
+++ p1final.c
@@ -4,8 +4,8 @@ void input(float *x1, float *y1, float *x2, float *y2)
 
 {
   printf("enter two points\n");
-  scanf("%f%f%f%f",x1,y1,x2,y2);
-}
+  scanf ("%f%f%f%f",x1,y1,x2,y2);
+    }
 void find_distance(float x1, float y1, float x2, float y2, float *distance)
 {
  *distance=sqrt(pow((x2-x2),2)+pow((y2-y1),2));
